e197b567948f4eef7d2b2aa247863e8a364c8bca
IAftabRehman <iamaftabrehman@gmail.com>
2025-05-19 20:09:32 +0500

    In this Commit Get Current Location From User

--- lib/UI/homeScreen.dart
+++ lib/UI/homeScreen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class homeScreen extends StatefulWidget {
   const homeScreen({super.key});
@@ -20,7 +21,7 @@ class _homeScreenState extends State<homeScreen> {
   );
 
   List<Marker> _marker = [];
-  List<Marker> _list = const [
+  List<Marker> _list =  [
     Marker(
       markerId: MarkerId('1'),
       position: LatLng(33.72993324248651, 73.03831168800549),
@@ -30,13 +31,14 @@ class _homeScreenState extends State<homeScreen> {
 
   String coordinatesResult = '';
   String addressResult = '';
+
   Future<void> getAddressFromCoordinates(double lat, double lon) async {
     try {
-      List<Placemark> placemarks = await placemarkFromCoordinates(lat, lon);
-      Placemark place = placemarks[0];
+      List<Placemark> placeMark = await placemarkFromCoordinates(lat, lon);
+      Placemark place = placeMark[0];
       setState(() {
         addressResult =
-        '${place.street}, ${place.locality}, ${place.postalCode}, ${place.country}';
+            '${place.street}, ${place.locality}, ${place.postalCode}, ${place.country}';
       });
       print(addressResult.toString());
     } catch (e) {
@@ -44,6 +46,17 @@ class _homeScreenState extends State<homeScreen> {
         addressResult = 'Error: $e';
       });
     }
+  }
+
+  Future<Position?> getCurrentLocation() async {
+    await Geolocator.requestPermission().then((value) {}).onError((
+      error,
+      stackTrace,
+    ) {
+      print("error" + error.toString());
+    });
+
+    return await Geolocator.getCurrentPosition();
   }
 
   @override
@@ -58,22 +71,39 @@ class _homeScreenState extends State<homeScreen> {
       appBar: AppBar(backgroundColor: Colors.blue, title: Text("Home Screen")),
       body: Column(
         children: [
-          Expanded(flex: 1,
-              child: Column(
-                children: [
-                  Text("Address is: ${addressResult.toString()}"),
-                  SizedBox(height: 5),
-                  ElevatedButton(
-                              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue, minimumSize: Size(20, 10),),
-                  onPressed: (){
-                                setState(() {
-                                  getAddressFromCoordinates(27.830131305009175, 79.30190530946963);
-                                });
-
+          Expanded(
+            flex: 1,
+            child: Column(
+              children: [
+                Text("Address is: ${addressResult.toString()}"),
+                SizedBox(height: 5),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    minimumSize: Size(20, 10),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      getAddressFromCoordinates(
+                        27.830131305009175,
+                        79.30190530946963,
+                      );
+                    });
                   },
-                  child: Center(child: Text("Click", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30)))),
-                ],
-              )),
+                  child: Center(
+                    child: Text(
+                      "Click",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
           Expanded(
             flex: 5,
             child: GoogleMap(
@@ -88,11 +118,31 @@ class _homeScreenState extends State<homeScreen> {
         ],
       ),
 
-      floatingActionButton: FloatingActionButton(onPressed: ()async{
-        GoogleMapController controller = await _controller.future;
-        controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(27.830131305009175, 79.30190530946963), zoom: 15)));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          getCurrentLocation().then((value) async {
+            print(value!.latitude.toString() + " " + value.latitude.toString());
+            _marker.add(
+              Marker(
+                markerId: MarkerId('2'),
+                position: LatLng(value.latitude, value.altitude),
+                infoWindow: InfoWindow(
+                  title: "My Current Location"
+                )
+              )
+            );
+            CameraPosition cameraPosition = CameraPosition(target: LatLng(value.latitude, value.altitude), zoom: 14);
 
-      }, child: Icon(Icons.location_on),),
+            final GoogleMapController controller = await _controller.future;
+
+            controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+            setState(() {
+
+            });
+          });
+        },
+        child: Icon(Icons.location_on),
+      ),
     );
   }
 }
